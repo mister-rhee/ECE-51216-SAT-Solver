a8d7123e465cc981bd9bc729c0013d92d80a8f23
Richard Rhee <rdrhee@gmail.com>
2026-03-21 14:14:10 -0700

    Wrap parser and print data into separate functions

--- dimacs_parser.py
+++ dimacs_parser.py
@@ -1,9 +1,13 @@
 from pysat.formula import CNF
 
-# Automatically parses the DIMACS CNF file
-cnf = CNF(from_file='uf20-91/uf20-01.cnf')
+def dimacs_parser(filepath):
+    # Automatically parses the DIMACS CNF file
+    cnf = CNF(from_file=filepath)
 
-# Access the parsed data
-print(f"Number of variables: {cnf.nv}")
-print(f"Number of clauses: {len(cnf.clauses)}")
-print(f"Clauses: {cnf.clauses}")
+    return cnf
+
+def print_parsed_data(parsed_cnf):
+    # Access the parsed data
+    print(f"Number of variables: {parsed_cnf.nv}")
+    print(f"Number of clauses: {len(parsed_cnf.clauses)}")
+    print(f"Clauses: {parsed_cnf.clauses}")
